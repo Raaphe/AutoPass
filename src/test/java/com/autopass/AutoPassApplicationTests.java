47d2f605fbb81971eb4f81cd25a68b9df15be58f
Raaphe <120033739+Raaphe@users.noreply.github.com>
2023-11-17 23:43:32 -0500

    Initial commit

---
+++ src/test/java/com/autopass/AutoPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
