7cf7f12031506a425e2c41f848bb2f3efbca7b6d
Aliterallamb <alitteralpotato@gmail.com>
2024-02-16 16:04:44 -0500

    Slight changes to the footer and added semantic to the project.
    Slight changes to the footer and added semantic to the project.

--- auto-pass-app/src/App.tsx
+++ auto-pass-app/src/App.tsx
@@ -34,8 +34,8 @@ const App = () => {
 
 
                 </Routes>
-                <Footer/>
             </div>
+            <Footer/>
             
         </Router>
     );
--- auto-pass-app/src/components/Footer/Footer.tsx
+++ auto-pass-app/src/components/Footer/Footer.tsx
@@ -8,19 +8,19 @@ interface FooterProps {
 const Footer: FC<FooterProps> = () => (
     <div className={styles.Footer}>
         <div className='row p-2 mt-4 '>
-            <p className='col-4'>Copyright Ⓒ 2024 AutoPass</p>
-            <img className='col-4 ' src='' alt='logo'/>
-            <a className='col'
+            <p className='col-2 text-light'>Copyright Ⓒ 2024 AutoPass</p>
+            <img className='col-7 ' src='' alt='logo'/>
+            <a className='col text-light'
             href='https://github.com/Raaphe'
             target='_blank'
             rel="noopener"
             aria-label='@Raaphe'><AiFillGithub />@Raaphe</a>
-            <a className='col'
+            <a className='col text-light'
             href='https://github.com/ikacef'
             target='_blank'
             rel="noopener"
             aria-label='@ikacef'><AiFillGithub />@ikacef</a>
-            <a className='col'
+            <a className='col text-light'
             href='https://github.com/AliteralLamb'
             target='_blank'
             rel="noopener"
